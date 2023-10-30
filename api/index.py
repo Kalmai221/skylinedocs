34e9b18a8f6b789fb3e33c2876e78df03fd4893b
Kalmai221 <hkurtislee@outlook.com>
2023-10-30 10:25:31 +0000

    Massive Update in code

--- api/index.py
+++ api/index.py
@@ -6,16 +6,23 @@ app = Flask(__name__)
 def home():
     shownews = True
     card_header = 'Latest News'
+    card_time = "30/10/2023"
     card_title = 'Flume V3.0.3 Release'
     card_text = 'This update comes with new Sever Utility Command!'
     card_link = '/'
     card_button_text = 'Go somewhere'
     
-    return render_template('home.html', shownews=shownews, card_header=card_header, card_title=card_title, card_text=card_text, card_link=card_link, card_button_text=card_button_text)
+    return render_template('home.html', shownews=shownews, card_header=card_header,card_time = card_time , card_title=card_title, card_text=card_text, card_link=card_link, card_button_text=card_button_text)
 
-@app.route('/documentation')
+@app.route('/docs')
 def documentation():
-    return render_template('documentation.html')
+    return render_template('docs.html')
+
+# Custom 404 error handler
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
 
 if __name__ == '__main__':
     app.run(debug=True)
